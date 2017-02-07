f600f4e76f3d3a2f4eb16e37754ac59271b2d6d4
Bryan Jackson <bjack13@gmail.com>
2017-02-07 14:56:04 -0700

    base the Component

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,14 +4,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <h1>The <strong>facebook</strong> Friend Machine</h1>
+
+          <div className="friends">
+
+          </div>
       </div>
     );
   }
